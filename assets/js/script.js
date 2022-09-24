6a1bfdcf502b50d7431d5946a94442fad1e73098
adityawahyuramadhan <awahyu148@gmail.com>
2022-09-24 23:58:48 +0700

    add carousel responsive for small device

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,24 @@
-// ketika melakukan scrolling, beri background pada navigation
-const navbar = document.querySelector(".navigation");
-
-window.onscroll = function () {
-  if (window.pageYOffset > navbar.offsetTop) {
-    navbar.classList.add("navbar-scrolling");
-  } else {
-    navbar.classList.remove("navbar-scrolling");
-  }
-};
-
-
 // init Swiper:
-const swiper = new Swiper('.swiper', {
-	effect: "coverflow",
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: "auto",
-	spaceBetween: 100,
-	loop: true,
-	coverflowEffect: {
-	  rotate: 0,
-	  stretch: 0,
-	  depth: 100,
-	  modifier: 1,
-	  slideShadows: false,
-	},
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
-	pagination: {
-	  el: ".swiper-pagination",
-	  clickable: true,
-	},
-  });
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 100,
+  loop: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  // autoplay: {
+  // 	delay: 3000,
+  // 	disableOnInteraction: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});

777f46b7323a1b32eec83616f39981f8f9c3a228
paul_lestyo <paulus.lestyo@student.uns.ac.id>
2022-09-24 22:03:12 +0700

    add title and floating button back to top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,34 @@
 // ketika melakukan scrolling, beri background pada navigation
 const navbar = document.querySelector(".navigation");
+let mybutton = document.getElementById("btn-back-to-top");
 
 window.onscroll = function () {
+  scrollFunction();
   if (window.pageYOffset > navbar.offsetTop) {
     navbar.classList.add("navbar-scrolling");
   } else {
     navbar.classList.remove("navbar-scrolling");
   }
 };
+
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.setProperty('display', 'block', 'important');
+  } else {
+    mybutton.style.setProperty('display', 'none', 'important');
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
 // init Swiper:
